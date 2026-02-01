188414a8af28a7fcbcf5b40eab62f2bc9da0eda8
sanket.pargaonkar <sanket.pargaonkar@gmail.com>
2026-02-01 18:04:29 +0530

    adds shells script to test apis

---
+++ api_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source .env
+baseUrl="http://localhost:${PORT}";
+echo -H "Authorisation: ${AUTH_HEADER}" "${baseUrl}/health" 
+curl  -H "Authorisation: ${AUTH_HEADER}" "${baseUrl}/health"
