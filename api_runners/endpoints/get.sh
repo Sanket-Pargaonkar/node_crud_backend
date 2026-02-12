2849cee58ef92e5ff7546a208869b500d37d5b07
sanket.pargaonkar <sanket.pargaonkar@gmail.com>
2026-02-12 15:45:52 +0530

    updates get.sh

--- api_runners/endpoints/get.sh
+++ api_runners/endpoints/get.sh
@@ -3,6 +3,6 @@
 source .env
 
 baseUrl="http://localhost:${PORT}";
-echo -H "Authorisation: ${AUTH_HEADER}" "${baseUrl}/health" 
+echo -H "Authorisation: ${AUTH_HEADER}" "${baseUrl}/" 
 curl  -X GET -H "Authorisation: ${AUTH_HEADER}" "${baseUrl}/"
 
